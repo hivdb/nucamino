db03c58d41d5c6aac72067e7e934a94709d9ac75
Philip Tzou <philiptz@stanford.edu>
2017-11-09 18:09:38 -0800

    Fix a path issue in hack/pprof.sh

--- hack/pprof.sh
+++ hack/pprof.sh
@@ -5,12 +5,12 @@ set -e
 cd /go/src/github.com/hivdb/nucamino
 ./_pprof/nucamino hiv1b --pprof --gene POL --input _pprof_input.fas -o /dev/null 2> /tmp/output
 PPROF_FILE=`grep "\(cpu\|memory\) profiling disabled" /tmp/output | awk '{print $(NF)}'`
-mkdir -p local
+mkdir -p _local
 if [ "$1" = "pdf" ]; then
-    go tool pprof -pdf ${PPROF_FILE} > local/pprof.pdf
+    go tool pprof -pdf ${PPROF_FILE} > _local/pprof.pdf
 else
     go tool pprof ${PPROF_FILE}
 fi
-echo "Profile result generated at ./local/pprof.pdf"
+echo "Profile result generated at ./_local/pprof.pdf"
 # clean up
 rm -rf _pprof
