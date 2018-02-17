470dd07ce639d70b9028f0b7f36447d9716ea9cb
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-02-16 16:05:36 -0800

    Factor HIV1B-specific procedures out of core CLI
    In preparation for adding HCV support, this commit factors
    HIV1B-specific features out of the core CLI procedure. This leaves the
    HIV1B specific configuration data in one module, with the the bulk of
    the common operations (IO, concurrency, argument wrangling, etc.) in a
    separate procedure.
    
    The resulting "architecture" is that the `cli/hiv1b` module (and other
    modules in the future) expose a `PerformAlignment` function that might
    perform some configuration or argument checking, but mostly pulls in
    extra information (such as reference sequences) and hands off to the
    core `PerformAlignment` function in `cli/cli`.
    
    To make passing arguments through the genome-specific
    `PerformAlignment` function less verbose, this commit also adds to
    structs for storing arguments, one for input and output arguments, and
    one for generic alignment parameters.

--- cli/hiv1b/hiv1b.go
+++ cli/hiv1b/hiv1b.go
@@ -1,20 +1,11 @@
 package hiv1b
 
 import (
-	"fmt"
-	"github.com/hivdb/nucamino/alignment"
 	cli "github.com/hivdb/nucamino/cli/cli"
-	d "github.com/hivdb/nucamino/data"
-	h "github.com/hivdb/nucamino/scorehandler/general"
-	a "github.com/hivdb/nucamino/types/amino"
-	"github.com/hivdb/nucamino/utils/fastareader"
-	"log"
-	"os"
-	"runtime"
-	"sync"
+	data "github.com/hivdb/nucamino/data"
 )
 
-var AllPositionalIndelScores = map[cli.Gene]map[int][2]int{
+var hiv1bPositionalIndelScores = cli.PositionalIndelScores{
 	cli.GAG: map[int][2]int{
 		111: [2]int{-5, 0},
 		112: [2]int{-5, 0},
@@ -180,129 +171,21 @@ var AllPositionalIndelScores = map[cli.Gene]map[int][2]int{
 	},
 }
 
-
 func PerformAlignment(
-	inputFileName string,
-	outputFileName string,
+	ioParams cli.IOParameters,
 	textGenes []string,
-	indelCodonOpeningBonus int,
-	indelCodonExtensionBonus int,
-	stopCodonPenalty int,
-	gapOpeningPenalty int,
-	gapExtensionPenalty int,
 	goroutines int,
-	outputFormat string, quiet bool) {
+	quiet bool,
+	alignmentParams cli.AlignmentParameters) {
 
-	runtime.LockOSThread()
-	numCPU := runtime.NumCPU()
-	logger := log.New(os.Stderr, "", 0)
-	if goroutines == 0 {
-		goroutines = numCPU
-	}
-	if !quiet {
-		logger.Printf(
-			"%d CPUs were detected. %d goroutines will be created.\n",
-			numCPU, goroutines)
-	}
-
-	var input, output *os.File
-	var err error
-	if inputFileName == "-" {
-		input = os.Stdin
-	} else {
-		input, err = os.Open(inputFileName)
-		if err != nil {
-			logger.Fatal(err)
-			return
-		}
-	}
-	if outputFileName == "-" {
-		output = os.Stdout
-	} else {
-		output, err = os.Create(outputFileName)
-		if err != nil {
-			logger.Fatal(err)
-			return
-		}
-	}
-
-	genesCount := len(textGenes)
-	genes := make([]cli.Gene, genesCount)
-	refs := make([][]a.AminoAcid, genesCount)
-	for i, textGene := range textGenes {
-		genes[i] = cli.GeneLookup[textGene]
-		refs[i] = d.HIV1BRefLookup[textGene]
-	}
-
-	var (
-		wg         = sync.WaitGroup{}
-		seqs       = fastareader.ReadSequences(input)
-		resultChan = make(chan []cli.AlignmentResult)
-		resultMap  = make(map[string][]cli.AlignmentResult)
-	)
-	if !quiet {
-		logger.Printf("%d sequences were found from the input file.\n", len(seqs))
-	}
-	var seqChan = cli.SeqSlice2Chan(seqs, goroutines*4)
-	for i := 0; i < goroutines; i++ {
-		wg.Add(1)
-		go func(idx int, rChan chan<- []cli.AlignmentResult) {
-			scoreHandlers := make([]*h.GeneralScoreHandler, genesCount)
-			for i, gene := range genes {
-				positionalIndelScores, isPositionalIndelScoreSupported := AllPositionalIndelScores[gene]
-				scoreHandlers[i] = h.New(
-					stopCodonPenalty,
-					gapOpeningPenalty,
-					gapExtensionPenalty,
-					indelCodonOpeningBonus,
-					indelCodonExtensionBonus,
-					positionalIndelScores,
-					isPositionalIndelScoreSupported)
-			}
-			for seq := range seqChan {
-				isSimpleAlignment := true
-				result := make([]cli.AlignmentResult, genesCount)
-				for i := 0; i < genesCount; i++ {
-					aligned, err := alignment.NewAlignment(seq.Sequence, refs[i], scoreHandlers[i])
-					if err != nil {
-						result[i] = cli.AlignmentResult{seq.Name, nil, err.Error(), err}
-					} else {
-						r := aligned.GetReport()
-						result[i] = cli.AlignmentResult{seq.Name, r, "", nil}
-						isSimpleAlignment = isSimpleAlignment && r.IsSimpleAlignment
-					}
-				}
-				rChan <- result
-				if !quiet {
-					if isSimpleAlignment {
-						fmt.Fprintf(os.Stderr, ":")
-					} else {
-						fmt.Fprintf(os.Stderr, ".")
-					}
-				}
-			}
-			wg.Done()
-		}(i, resultChan)
-	}
-	go func(rChan chan<- []cli.AlignmentResult) {
-		wg.Wait()
-		if !quiet {
-			logger.Printf("\n")
-		}
-		close(rChan)
-	}(resultChan)
-	for result := range resultChan {
-		resultMap[result[0].Name] = result
-	}
-	switch outputFormat {
-	case "tsv":
-		cli.WriteTSV(output, textGenes, seqs, resultMap)
-		break
-	case "json":
-		cli.WriteJSON(output, textGenes, seqs, resultMap)
-		break
-	}
-	if !quiet && outputFileName != "-" {
-		logger.Printf("Created alignment result file %s.", outputFileName)
-	}
+	cli.PerformAlignment(
+		ioParams.InputFileName,
+		ioParams.OutputFileName,
+		ioParams.OutputFormat,
+		textGenes,
+		goroutines,
+		quiet,
+		alignmentParams,
+		hiv1bPositionalIndelScores,
+		data.HIV1BRefLookup)
 }
