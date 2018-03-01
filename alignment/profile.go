438bef3e9eeee52a809d05e3139ba90fd14618da
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-02-28 16:27:50 -0800

    Add helper functions to AlignmentProfile
    This adds an abstraction for checking for positional indel score
    support and getting the list of genes that a profile supports.

--- alignment/profile.go
+++ alignment/profile.go
@@ -1,13 +1,41 @@
 package alignment
 
+import (
+	a "github.com/hivdb/nucamino/types/amino"
+)
+
 type Gene string
 
 type PositionalIndelScores map[Gene]map[int]([2]int)
 
+// This stores the all the information needed to align a sequence
+// against a particular reference: alignment parameters, positional
+// in-del scores, and the reference sequences to be aligned against.
 type AlignmentProfile struct {
 	StopCodonPenalty         int
 	GapOpenPenalty           int
 	IndelCodonOpeningBonus   int
 	IndelCodonExtensionBonus int
-	PositionalIndelScores    map[Gene]PositionalIndelScores
+	GeneIndelScores          PositionalIndelScores
+	ReferenceSequences       map[Gene][]a.AminoAcid
+}
+
+func (profile *AlignmentProfile) SupportsPositionalIndelScores() bool {
+	return profile.GeneIndelScores != nil
+}
+
+func (profile *AlignmentProfile) SupportsPositionalIndelScoresFor(gene Gene) bool {
+	if ! profile.SupportsPositionalIndelScores() {
+		return false
+	}
+	_, hasKey := profile.GeneIndelScores[gene]
+	return hasKey
+}
+
+func (profile *AlignmentProfile) Genes() []Gene {
+	var genes = make([]Gene, 0, len(profile.ReferenceSequences))
+	for gene, _ := range profile.ReferenceSequences {
+		genes = append(genes, gene)
+	}
+	return genes
 }
