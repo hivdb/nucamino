6b4dd878e55c5bd0a999f312afdca5514015730a
Nathaniel Knight <nknight@cfenet.ubc.ca>
2018-03-15 15:53:11 -0700

    Update hack scripts to reflect new CLI

--- hack/build.sh
+++ hack/build.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
 set -e
-BUILDFOLDER=${1:-"build"} 
+BUILDFOLDER=${1:-"build"}
+if [ -z $GOPATH ]; then
+    GOPATH=${HOME}/go  # Enable default GOPATH (as per https://rakyll.org/default-gopath/)
+fi
 
-cd /go/src/github.com/hivdb/nucamino
+cd ${GOPATH}/src/github.com/hivdb/nucamino
 go get ./...
 mkdir -p $BUILDFOLDER
 cd $BUILDFOLDER
-echo "`go build -gcflags="-l=4" -compiler="gc" -v github.com/hivdb/nucamino/cmd/nucamino 2>&1` => ./$BUILDFOLDER/nucamino"
+echo "`go build -gcflags="-l=4" -compiler="gc" -v github.com/hivdb/nucamino 2>&1` => ./$BUILDFOLDER/nucamino"
